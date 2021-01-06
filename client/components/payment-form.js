2d85a9619ef48197a279c686fa1655d31fc9a5cd
akshay <akshayd915@gmail.com>
2021-01-06 16:14:00 +0530

    aknjidan

--- client/components/payment-form.js
+++ client/components/payment-form.js
@@ -1,28 +1,41 @@
-import React from 'react'
-import StripeCheckout from 'react-stripe-checkout';
+import React, { Component } from 'react';
 
-export default class PaymentForm extends React.Component {
-  constructor(props) {
-    super(props);
+class RazorPanel extends Component {
+    constructor(props) {
+        super(props)
 
-    this.state = {}
+    }
+    RequestOrderPayment(){
+                var options = {
+                    "key_id": "rzp_test_BXuuvSnv4i88g9",
+                    "key_secret": "UwnwdougBKgb4Z5Vl0zMiJq6",
+                    "amount": '2000',
+                    "currency": "INR",
+                    "name": "Acme Corp",
+                    "description": "A Wild Sheep Chase is the third novel by Japanese author  Haruki Murakami",
+                    "order_id": 'order_GLzK9bK7ltYbK7',
+                    "prefill": {
+                        "name": "Gaurav Kumar",
+                        "email": "gaurav.kumar@example.com",
+                        "contact": "9999999999",
+                    },
+                    "notes": {
+                        "address": "note value",
+                    },
+                    "theme": {
+                        "color": "#F37254"
+                    }
+                };
+                var rzp1 = new window.Razorpay(options);
+                rzp1.open();
+            }
 
-    this.onToken = this.onToken.bind(this);
-  }
-
-  onToken(token) {
-    this.props.callback(token);
-  }
-
-  // ...
-
-  render() {
-    return (
-      // ...
-      <StripeCheckout
-        token={this.onToken}
-        stripeKey="pk_test_BuN9Y2sdSBcUOWqrV20QeMXE"
-      />
-    )
-  }
+    render() {
+        return (
+            <div>
+                <button id="rzp-button1" onClick={() => this.RequestOrderPayment()}>Pay</button>
+            </div>
+        )
+    }
 }
+export default RazorPanel;
